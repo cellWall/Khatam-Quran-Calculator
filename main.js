c3a7e6085cd994298937f7e879d1bfeea81f7a94
cellWall <salwasanaullahkhan@gmail.com>
2019-05-25 15:45:12 +0800

    i made the js able to detect if a number that is too big is in the input

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 function calculateDaysNeeded() {
     const numberOfPages = document.getElementById("numberOfPages").value
     const daysNeeded = 604 / numberOfPages
-    document.getElementById("answer").innerHTML = daysNeeded
+
+    let message = 'you need ' + daysNeeded + ' days to read.';
+
+    if (daysNeeded <= 365) {
+        message += "Well done! you are going to finish the Quran in less than a year!!"
+    } else {
+       message += "Do not worry, with patience comes success!"
+    }
+
+    if (numberOfPages > 604) {
+        message = "there are only 604 pages in the Qur'an."
+    } 
+    
+
+    document.getElementById('answer').innerHTML = message
 }
+
